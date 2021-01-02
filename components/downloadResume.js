210b2310737cceb83599a6ad6bcfb683925d2690
Mateusz Delegacz <mat.delegacz@gmail.com>
2021-01-02 22:48:40 +0000

    download resume btn added

--- components/downloadResume.js
+++ components/downloadResume.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect} from 'react'
 import Link from 'next/link'
 import classNames from "classnames";
+import { Icon, InlineIcon } from '@iconify/react';
+import arrowDownCircleFill from '@iconify/icons-bi/arrow-down-circle-fill';
 
 
-const btnClasses = classNames(
-    'dwnld'
-)
 
 export default function DownloadResume() {
+    const [isHovered, setHover] = useState(false)
+    useEffect(() => {},[])
+    const btnClasses = classNames(
+        'dwnld',
+        {
+            "dwnld--hovered": isHovered
+        }
+    )
     return(
         <button className={ btnClasses }>
-            <Link href="/sample.pdf"><a target="_blank">Download Resume</a></Link>
+            <Link href="/sample.pdf">
+                <div style={ {display: "flex", alignItems: "center"}}>
+                    <a target="_blank">DOWNLOAD RESUME</a>
+                    <span style={{marginLeft:16, marginTop:5}}><Icon icon={arrowDownCircleFill} style={{color: '#ffffff', fontSize: '28px'}} /></span>
+                </div>
+            </Link>
         </button>
     )
 }
