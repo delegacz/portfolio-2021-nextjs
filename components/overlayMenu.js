90fbaa6c591ae2371dabd92489972ca97b1ac64b
Mateusz Delegacz <mat.delegacz@gmail.com>
2021-05-16 23:45:53 +0100

    some overlay menu styling

--- components/overlayMenu.js
+++ components/overlayMenu.js
@@ -1,8 +1,24 @@
 import React, {useState, useEffect} from 'react'
 import { Grid } from '@material-ui/core'
 import {Link} from 'next/link'
+import { render } from 'react-dom'
 export default function OverlayMenu(props) {
     
+    const menuItems = [
+        {
+            name: 'Work',
+            url: '/'
+        },
+        {
+            name: 'About',
+            url: '/about'
+        },
+        {
+            name: 'Contact',
+            url: '/contact'
+        },
+    ]
+
     function hideShow(state) {
         if(state) {
             return {visibility: 'visible'}
@@ -11,22 +27,51 @@ export default function OverlayMenu(props) {
             return {visibility: 'hidden'}
         }
     }
+
     
 
     return (
        <section className="overlay-menu" style={hideShow(props.isVisible)}>
-           <ul>
-               <li>
-                   Work
-                </li>
-               <li>
-                   About
-                </li>
-               <li>
-                   Contact
-                </li>
-           </ul>
-           <button class="overlay-menu-exit" onClick={props.changeVisibility}>x</button>
+        <Menu menuItems={ menuItems } />
+        <CloseButton changeVisibility={props.changeVisibility}/>
        </section>
+    )
+}
+
+const Menu = ({ menuItems }) => {
+    const menuStyle = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        height: '100%'
+    }
+
+    return(
+        <ul style={menuStyle}>
+            {
+                menuItems.map(item => {
+                    return(
+                        <MenuItem item={item}/>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+const MenuItem = ({ item }) => {
+    return (
+       <li>
+            <a href={item.url}>
+                {item.name}
+            </a>
+        </li>
+    )
+}
+
+const CloseButton = (props) => {
+    return(
+        <button class="overlay-menu-exit" onClick={props.changeVisibility}>x</button>
     )
 }
